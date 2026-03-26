db2fc1bb645ee06cc40d28282caded9136cf0913
h0x91B <h0x91b@gmail.com>
2026-03-26 14:28:11 +0200

    v1.3.2

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "dev-3.0",
 		identifier: "dev3.electrobun.dev",
-		version: "1.3.1",
+		version: "1.3.2",
 	},
 	release: {
 		baseUrl: "https://h0x91b-releases.s3.eu-west-1.amazonaws.com/dev-3.0",
